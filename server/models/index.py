3628f8ebb0b21a3995fbef47dcbc1b673cca2484
Jordan Elliott <148821877+Auctic-Flipper@users.noreply.github.com>
2025-10-27 10:02:44 -0500

    Delete server/models directory

--- server/models/index.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-# Main site pages (home)
-bp = Blueprint("home", __name__)
-
-@bp.route("/")
-def index():
-    # Renders dashboard/index.html via Flask's template_folder setting
-    return render_template("index.html")
